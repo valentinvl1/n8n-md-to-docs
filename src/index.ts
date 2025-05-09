e6c7ca2c0aabe6a5c0613789bbfd1cb44925df17
valentinvl1 <lopesvalentinvl1@gmail.com>
2025-05-09 17:57:25 +0200

    Update limit size 50mb - index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,8 @@ import { convertMarkdownToDocx } from './services/docxConverter';
 const app = express();
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({ origin: true }));
 
 app.post('/', async (req: Request, res: Response) => {
